69b2d3c490b9fd6172027be078ca25ba6f6bfd16
contedenis <contedenis@outlook.com>
2020-02-19 22:06:19 -0300

    add react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+// @packages
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// @app
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const MOUNT_NODE = document.getElementById('root');
+ReactDOM.render((
+  <Router>
+    <App />
+  </Router>
+), MOUNT_NODE);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
