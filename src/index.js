eb30a235a81782299d367c51084de1d446fba4fa
contedenis <contedenis@outlook.com>
2020-02-20 15:36:45 -0300

    add babel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 // @packages
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
