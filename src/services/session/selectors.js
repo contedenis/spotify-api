0b7a1b2f9518f665d0c4e62a813e51b3ae80ad64
contedenis <contedenis@outlook.com>
2020-05-10 20:45:07 -0300

    update session selectors

--- src/services/session/selectors.js
+++ src/services/session/selectors.js
@@ -5,7 +5,8 @@ import { get } from 'lodash';
 import { LOGGED_OUT } from './constants';
 
 export const selectSessionState = ({ session }) => get(session, 'sessionState', LOGGED_OUT);
-export const selectUserCountry = ({ session }) => get(session, 'user.country');
-export const selectUserId = ({ session }) => get(session, 'user.id');
+export const selectUserCountry = ({ session }) => get(session, 'user.country', '');
+export const selectUserFetching = ({ session }) => get(session, 'loading', false);
+export const selectUserId = ({ session }) => get(session, 'user.id', '');
 export const selectUserImage = ({ session }) => get(session, 'user.images[0].url', '');
-export const selectUserName = ({ session }) => get(session, 'user.display_name');
+export const selectUserName = ({ session }) => get(session, 'user.display_name', '');
