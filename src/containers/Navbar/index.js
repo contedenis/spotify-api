7cc3d5e4493ff97fd42a04ae1521cba61712aabe
contedenis <contedenis@outlook.com>
2020-05-10 20:47:07 -0300

    add Navbar component

---
+++ src/containers/Navbar/index.js
@@ -0,0 +1,74 @@
+// @packages
+import React, { useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+// @app
+import Image from 'components/Image';
+import {
+  selectUserFetching,
+  selectUserImage,
+  selectUserName,
+} from 'services/session/selectors';
+import Spinner from 'components/Spinner';
+import useClickOutside from 'hooks/useClickOutside';
+import { initLogoutProcess } from 'services/session/actions';
+import { useAuthContext } from 'components/Context/Auth';
+
+// @own
+import {
+  Chip,
+  NavbarStyled,
+  NameText,
+  Popup,
+  PopupText,
+} from './styles';
+
+function Navbar({
+  initLogoutProcess,
+  loading,
+  name,
+  src,
+}) {
+  const { onLogout } = useAuthContext();
+  const [open, setOpen] = useState(false);
+  const navbarRef = useRef(null);
+  useClickOutside(navbarRef, () => setOpen(false));
+
+  function handleOnClick() {
+    initLogoutProcess({ key: 'token', onLogout });
+  }
+
+  return (
+    <NavbarStyled>
+      <Chip onClick={() => setOpen(!open)}>
+        <Spinner loading={loading} size={15}>
+          <Image src={src} size={30} type="circle" />
+          <NameText>{name}</NameText>
+        </Spinner>
+      </Chip>
+      {open && (
+        <Popup onClick={handleOnClick} ref={navbarRef}>
+          <PopupText>
+            Log out
+          </PopupText>
+        </Popup>
+      )}
+    </NavbarStyled>
+  );
+}
+
+Navbar.propTypes = {
+  initLogoutProcess: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  loading: selectUserFetching(state),
+  name: selectUserName(state),
+  src: selectUserImage(state),
+});
+
+export default connect(mapStateToProps, { initLogoutProcess })(Navbar);
---
+++ src/containers/Navbar/styles.js
@@ -0,0 +1,78 @@
+// @packages
+import styled, { keyframes } from 'styled-components';
+
+const NavbarStyled = styled.div`
+  background: #1C2833;
+  display: flex;
+  justify-content: flex-end;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+`;
+
+const Chip = styled.div`
+  background: rgb(232, 232, 232, 0.5);
+  border-radius: 50px;
+  cursor: pointer;
+  display: flex;
+  margin: 5px 20px 5px 5px;
+  min-width: 100px;
+  padding: 5px 10px 5px 5px;
+  position: relative;
+
+  &:hover {
+    background: rgb(232, 232, 232, 0.8);
+    transition: background .2s ease-in;
+  }
+`;
+
+const NameText = styled.h5`
+  align-items: center;
+  color: #123622;
+  display: flex;
+  font-family: 'Roboto', sans-serif;
+  font-size: 14px;
+  justify-content: center;
+  margin: 0;
+  margin-left: 10px;
+`;
+
+export const PopupShow = keyframes`
+  from {
+    top: 45px;
+    opacity: 0;
+  }
+  to {
+    top: 55px;
+    opacity: 1;
+  }
+`;
+
+const Popup = styled.div`
+  animation: ${PopupShow} .2s;
+  background: #1C2833;
+  border-radius: 5px;
+  border: 0;
+  position: absolute;
+  right: 20px;
+  top: 55px;
+  width: 100px;
+`;
+
+const PopupText = styled.h5`
+  cursor: pointer;
+  font-family: 'Roboto', sans-serif;
+  font-size: 14px;
+  margin: 0px;
+  padding: 10px;
+  text-align: center;
+`;
+
+export {
+  Chip,
+  NameText,
+  NavbarStyled,
+  Popup,
+  PopupText,
+};
