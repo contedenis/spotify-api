0523789ddb94d15b32cf8dbfc3c5d2aa7bf325d3
contedenis <contedenis@outlook.com>
2020-05-12 18:37:12 -0300

    update Home with RecentlyListened

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // @app
 import Layout from 'containers/Layout';
 import Navbar from 'containers/Navbar';
+import RecentlyListened from 'containers/RecentlyListened';
 import UserCard from 'containers/UserCard';
 
 // @own
 import { GridStyled } from './styles';
 
 function Home() {
+  const [showRecently, setShowRecently] = useState(false);
+
   return (
     <Layout>
       <Navbar />
       <GridStyled>
-        <UserCard />
+        <UserCard onAnimationEnd={() => setShowRecently(true)} />
+        {showRecently && <RecentlyListened />}
       </GridStyled>
     </Layout>
   );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 
 const GridStyled = styled.div`
   padding: 80px 20px;
+  width: calc(100% - 40px);
+  display: flex;
 `;
 
 export { GridStyled };
