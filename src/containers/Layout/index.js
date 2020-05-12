9cc66b463643a3596a43172d1f33ecdc4665ab0b
contedenis <contedenis@outlook.com>
2020-05-12 10:11:57 -0300

    update Layout with FullscreenVideo

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -2,12 +2,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// @app
+import VideoFullscreen from 'components/FullscreenVideo';
+import guitarPoster from 'assets/images/guitarPoster.jpg';
+import guitarVideo from 'assets/video/guitarMp4.mp4';
+
 // @own
 import { LayoutStyled } from './styles';
 
 function Layout({ children }) {
   return (
     <LayoutStyled>
+      <VideoFullscreen poster={guitarPoster} src={guitarVideo} />
       {children}
     </LayoutStyled>
   );
--- src/containers/Layout/styles.js
+++ src/containers/Layout/styles.js
@@ -2,14 +2,10 @@
 import styled from 'styled-components';
 
 const LayoutStyled = styled.div`
-  align-items: center;
-  background-color: #9ACD32;
+  align-items: flex-start;
   color: white;
   display: flex;
   flex-direction: column;
-  font-size: calc(10px + 2vmin);
-  justify-content: center;
-  min-height: 100vh;
 `;
 
 export { LayoutStyled };
