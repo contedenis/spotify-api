7485da592cd00a39832f7732aa08c48b7f6b4556
contedenis <contedenis@outlook.com>
2020-05-13 10:13:11 -0300

    support className in Image

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -5,18 +5,30 @@ import PropTypes from 'prop-types';
 // @own
 import { ImageStyled } from './styles';
 
-function Image({ size, src, type }) {
+function Image({
+  className,
+  size,
+  src,
+  type,
+}) {
   return (
-    <ImageStyled size={size} src={src} type={type} />
+    <ImageStyled
+      className={className}
+      size={size}
+      src={src}
+      type={type}
+    />
   );
 }
 
 Image.defaultProps = {
-  type: 'circle',
+  className: '',
   size: 50,
+  type: 'circle',
 };
 
 Image.propTypes = {
+  className: PropTypes.string,
   size: PropTypes.number,
   src: PropTypes.string.isRequired,
   type: PropTypes.string,
