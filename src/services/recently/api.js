d7e6d262ab02f94488407a456ecf7fa831c25ad5
contedenis <contedenis@outlook.com>
2020-05-13 09:43:31 -0300

    modify requestHelpers and api call in recently

--- src/services/recently/api.js
+++ src/services/recently/api.js
@@ -3,8 +3,12 @@ import { spotifyApi } from 'utils/requestHelper';
 import { errorHandler } from 'utils/errorHandler';
 
 export function getRecentlyPlayed() {
+  const token = localStorage.getItem('token');
   return spotifyApi({
     url: 'me/player/recently-played',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
     params: {
       type: 'track',
       limit: 50,
--- src/utils/requestHelper.js
+++ src/utils/requestHelper.js
@@ -1,25 +1,14 @@
 // @ Packages
 import Axios from 'axios';
 
-const token = localStorage.getItem('token');
-
-const buildHeaders = () => {
-  let headers = {};
-  if (token) {
-    headers = {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    };
-  }
-  return headers;
-};
-
 const createAxiosInstance = () => {
   const instance = Axios.create({
     baseURL: 'https://api.spotify.com/v1',
     timeout: 30000,
-    headers: buildHeaders(),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
   });
   return instance;
 };
