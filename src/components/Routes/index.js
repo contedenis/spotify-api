5246b256c33444ec3bc50bf1ac8b250345f281d0
contedenis <contedenis@outlook.com>
2020-05-17 20:04:16 -0300

    migrate from Home to Me

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -3,13 +3,18 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 // @app
-import Home from 'pages/Home';
+import Me from 'pages/Me';
 import PrivateRoute from 'components/PrivateRoute';
 
 function Routes() {
+  const meRoutes = [
+    '/me',
+    '/me/playlists/:id',
+    '/me/recent-played/:id',
+  ];
   return (
     <Switch>
-      <PrivateRoute exact path="/home" component={Home} />
+      <PrivateRoute path={meRoutes} component={Me} />
       <PrivateRoute exact path="/callback" component={() => <div>Hello world!</div>} />
       <Route render={() => <h1>404: Page not found</h1>} />
     </Switch>
